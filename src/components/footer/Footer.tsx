1e87c601bfe92223351ade1e202018ac3f864c8b
tarchevsky <i.tarchevsky@gmai.com>
2024-10-14 14:36:07 +0300

    Адаптация footer (flex)

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const Footer = () => {
 	return (
-		<footer className='flex justify-between items-center cont p-10'>
+		<footer className='flex flex-col-reverse xl:flex-row justify-between items-center cont p-10'>
 			<Link href='/' className='text-5xl font-medium'>
 				Tezisc<span style={{ fontFamily: 'Poiret One, sans-serif' }}>Q</span>m
 			</Link>
